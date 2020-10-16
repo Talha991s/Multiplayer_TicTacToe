1896cf1ebb41341aee4ab63b0fc8e9fdb3742890
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-10-16 02:09:57 -0700

    Completed Login Details, Fixing bugs. M1 done!

---
+++ Assets/Scripts/LoginDetails.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginDetails : MonoBehaviour
+{
+    public GameObject rb;
+    public GameObject username;
+    public GameObject userinput;
+    public GameObject password;
+    public GameObject passwordInput;
+    public GameObject oginbutton;
+
+    public void LoginButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void registreButton()
+    {
+        rb.SetActive(true);
+    }
+
+    public void QuitLog()
+    {
+        Application.Quit();
+    }
+
+    public void credentials()
+    {
+        if (userinput == username && passwordInput == password)
+        {
+            oginbutton.SetActive(true);
+
+        }
+    }
+
+}
